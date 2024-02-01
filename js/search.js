c438469fdd128e261c25d4d17eca748d4b5236a9
DAHYUNdahyun <dahyun345@naver.com>
2024-02-02 02:20:22 +0900

    modify_search_dh

--- js/search.js
+++ js/search.js
@@ -4,3 +4,19 @@ const searchBox = document.querySelector('.search');
 searchBtn.addEventListener("focus", function() {
   searchBox.style.boxShadow = "0 0 3px gray";
 });
+
+const genre_box = document.querySelectorAll('.genre_box');
+
+for (let i = 0; i < genre_box.length; i++) {
+  genre_box[i].addEventListener('click', function() {
+    const correspondingAnswer = genre_box[i];
+
+    genre_box.forEach(genreboxElement => {
+      if (genreboxElement !== correspondingAnswer) {
+        genreboxElement.classList.remove('active');
+      }
+    });
+
+    correspondingAnswer.classList.toggle('active');
+  });
+}
