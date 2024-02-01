7f165016272e5bf1376ea528869e570af56362c2
unknown <dsmamkm@gmail.com>
2024-02-01 12:09:23 +0900

    modify_option_최종완료_min

--- js/option.js
+++ js/option.js
@@ -34,9 +34,12 @@ const btnWhite = document.getElementById("white");
 
 btnBlack.addEventListener("click", (e) => {
     document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
 });
 
 btnWhite.addEventListener("click", (e) => {
     document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
 });
+
 
